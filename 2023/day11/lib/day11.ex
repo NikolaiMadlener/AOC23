57fc9d6aaa52e891bb9e2801afda305e157360f6
NikolaiMadlener <nikolai@madlener.io>
2023-12-11 18:47:51 +0100

    day11 cleanup

--- 2023/day11/lib/day11.ex
+++ 2023/day11/lib/day11.ex
@@ -3,12 +3,11 @@ defmodule Day11 do
   Documentation for `Day11`.
   """
 
-  def sum1 do
+  # task1: 2, task2: 1_000_000
+  def sum(empty_factor) do
     {mapping, matrix} =
       read_file()
       |> parse_string()
-      |> transform_lines(2)
-      |> transform_rows(2)
       |> numberize()
 
     IO.inspect(mapping, label: "Mapping")
@@ -17,63 +16,34 @@ defmodule Day11 do
     pairs = pairs(matrix)
     IO.inspect(pairs, label: "Pairs")
 
-    lengths_sum(pairs, mapping)
+    lengths_sum(pairs, mapping, matrix, empty_factor)
   end
 
-  def sum2 do
-    {mapping, matrix} =
-      read_file()
-      |> parse_string()
-      |> transform_lines(1)
-      |> transform_rows(1)
-      |> numberize()
-
-    IO.inspect(mapping, label: "Mapping")
-    pairs = pairs(matrix)
-    IO.inspect(Enum.count(pairs), label: "Pairs")
-
-    lengths_sum_2(pairs, mapping, matrix)
-  end
-
-  defp lengths_sum(pairs, mapping) do
-    # iterate over pairs and find distance between them by looking up both positions in matrix
-    Enum.reduce(pairs, 0, fn {p1, p2}, acc ->
-      acc + distance(p1, p2, mapping)
-    end)
-  end
-
-  defp lengths_sum_2(pairs, mapping, matrix) do
+  defp lengths_sum(pairs, mapping, matrix, empty_factor) do
     # iterate over pairs and find distance between them by looking up both positions in matrix
     pairs
     |> Enum.with_index()
     |> Enum.reduce(0, fn {{p1, p2}, index}, acc ->
       progress = index / Enum.count(pairs) * 100
       IO.inspect(progress, label: "Progress %")
-      acc + distance_2(p1, p2, mapping, matrix)
+      acc + distance(p1, p2, mapping, matrix, empty_factor)
     end)
   end
 
-  defp distance_2(p1, p2, mapping, matrix) do
-    # lookup in map
+  defp distance(p1, p2, mapping, matrix, empty_factor) do
     p1 = Map.get(mapping, p1)
     p2 = Map.get(mapping, p2)
 
     empty_lines_indexes = matrix |> empty_indexes()
     empty_rows_indexes = matrix |> convert_matrix() |> empty_indexes()
 
-    # IO.inspect(empty_lines_indexes, label: "Empty lines")
-    # count how many empty lines are between p1.x and p2.x
-    # IO.inspect({p1, p2})
-
     empty_lines = count_empty_lines(p1, p2, empty_lines_indexes)
-    # manhatten distance
     empty_rows = count_empty_rows(p1, p2, empty_rows_indexes)
 
+    # manhatten distance
     distance =
-      abs(p1.x - p2.x) + empty_rows * (1_000_000 - 1) + abs(p1.y - p2.y) +
-        empty_lines * (1_000_000 - 1)
-
-    # IO.inspect(distance, label: "distance")
+      abs(p1.x - p2.x) + empty_rows * (empty_factor - 1) + abs(p1.y - p2.y) +
+        empty_lines * (empty_factor - 1)
   end
 
   defp count_empty_lines(p1, p2, empty_lines_indexes) when p1.x < p2.x do
@@ -96,30 +66,14 @@ defmodule Day11 do
     matrix
     |> Enum.with_index()
     |> Enum.reduce([], fn {line, index}, acc ->
-      # IO.inspect(acc)
-      # IO.inspect(line)
-
       if Enum.all?(line, fn x ->
-           if is_integer(x) do
-             false
-           else
-             x == "."
-           end
+           if is_integer(x), do: false, else: x == "."
          end) do
         Enum.concat(acc, [index])
       else
         acc
       end
     end)
-  end
-
-  defp distance(p1, p2, mapping) do
-    # lookup in map
-    p1 = Map.get(mapping, p1)
-    p2 = Map.get(mapping, p2)
-
-    # manhatten distance
-    abs(p1.x - p2.x) + abs(p1.y - p2.y)
   end
 
   defp pairs(matrix) do
@@ -173,9 +127,6 @@ defmodule Day11 do
       matrix
       |> Enum.with_index()
       |> Enum.map(fn {line, index} ->
-        {line, index}
-      end)
-      |> Enum.map(fn {line, index} ->
         {index, Enum.find_index(line, fn x -> x == element end)}
       end)
       |> Enum.filter(fn {row, col} ->
@@ -184,49 +135,8 @@ defmodule Day11 do
       |> Enum.at(0)
   end
 
-  defp transform_lines(matrix, amount) do
-    line_length = Enum.count(Enum.at(matrix, 0))
-
-    matrix =
-      matrix
-      |> Enum.map(fn line ->
-        dupl_line = line
-
-        if Enum.all?(line, fn x -> x == "." end) do
-          # concat line #amount of times
-          dupl_line =
-            Enum.reduce(1..amount, [], fn _, acc ->
-              Enum.concat(acc, line)
-            end)
-        else
-          [line]
-        end
-      end)
-      |> Enum.map(fn line ->
-        List.flatten(line)
-      end)
-      |> Enum.map(fn line ->
-        if Enum.count(line) > line_length do
-          # split line
-          line
-          |> Enum.chunk_every(line_length)
-        else
-          line
-        end
-      end)
-      |> List.flatten()
-      |> Enum.chunk_every(line_length)
-  end
-
-  defp transform_rows(matrix, amount) do
-    matrix
-    |> convert_matrix()
-    |> transform_lines(amount)
-    |> convert_matrix()
-  end
-
   defp convert_matrix(matrix) do
-    # convert matrix (switch rows and lines)
+    # invert matrix (switch rows and lines)
     Enum.zip_with(matrix, &Function.identity/1)
   end
 
@@ -242,19 +152,6 @@ defmodule Day11 do
   end
 
   defp read_file() do
-    # """
-    # ...#......
-    # .......#..
-    # #.........
-    # ..........
-    # ......#...
-    # .#........
-    # .........#
-    # ..........
-    # .......#..
-    # #...#.....
-    # """
-
     File.read!("/Users/madlener/git/AOC23/2023/day11/lib/input-11.txt")
   end
 end
